36f767e498a1b9294d16c1cff6de1577a6238341
Mo Aboumerhi <hamedaboumerhi@yahoo.com>
2017-03-23 22:56:30 -0400

    First commit

---
+++ MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStore.Models;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>
+            {
+                new Genre { Name = "Disco"},
+                new Genre {Name = "Jazz" },
+                new Genre {Name = "Rock" }
+            };
+
+            return View(genres);
+        }
+
+        //GET: /Store/Browse?genre=Disco
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+
+            return View(genreModel);
+        }
+
+        //Get: /Store/Details/5
+
+        public ActionResult Details(int id)
+        {
+            //string message = "Store.Details, ID = " + id;
+
+            //return message;
+            var album = new Album { Title = "Album" + id };
+
+            return View(album);
+        }
+    }
+}
